94a2ac58fb1e86c5968775b67215c93c224cd245
extav <143948204+extav@users.noreply.github.com>
2024-11-27 10:27:44 -0800

    Move ingredients object to its own module

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,6 @@
 "use strict";
 
-const tinga = {
-
-    "onion" : 1,
-    "garlic" : 3,
-    "boxes seitan" : 3,
-    "can tomatoes" : 1,
-    "canned chipotle peppers" : 3,
-    "tsp salt" : 3,
-    "tsp cumin" : 1,
-    "tsp oregano" : 1
-};
+import { tinga } from "./ingredients.js";
 
 const recipeObj = {
     "tinga" : {
